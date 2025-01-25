86a6bb74b1d66f8f07f1a0e2f7f4fe5f272bc644
Rodrigo Everton <rodrigoeverton1500@gmail.com>
2025-01-24 22:18:44 -0300

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let listaAmigos = document.getElementById("listaAmigos");
+
+function adicionarAmigo() {    
+    let novoAmigo = document.getElementById("amigo");
+    if(novoAmigo.value == "") {
+        alert("Insira o nome de um amigo");
+        return;
+    }
+
+    amigos.push(novoAmigo.value);
+    listarAmigos(novoAmigo);
+}
+
+function listarAmigos(novoAmigo) {
+    let novoLi = document.createElement("li");
+    novoLi.textContent = novoAmigo.value;
+    listaAmigos.appendChild(novoLi);
+    novoAmigo.value = "";
+}
+
+function sortearAmigo() {
+    let indice = parseInt(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indice];
+    let resultado = document.getElementById("resultado");
+    resultado.textContent = "Amigo secreto: " + amigoSorteado;
+
+    limparLista();
+}
+
+function limparLista() {
+    listaAmigos.innerHTML = "";
+    amigos = [];
+}
